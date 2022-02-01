c72a9a2a55c1ac1c725764a68ee35dcc566961bb
Sagar Patil <sagarreddypatil@gmail.com>
2022-02-01 01:47:07 -0500

    create field elements except donut

---
+++ Assets/Scripts/ImperialGrid.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class ImperialGrid : MonoBehaviour
+{
+    void Update()
+    {
+        float m2f = 3.280839895f;
+        float f2m = 1f / m2f;
+        Color color = Color.red;
+
+        Vector3 offset = new Vector3(-6f * f2m, 0, -6f * f2m) + transform.position;
+
+        // draw a 6x6 grid where each square is 2x2 feet
+        for (int i = 0; i < 6; i++)
+        {
+            for (int j = 0; j < 6; j++)
+            {
+                // draw a square
+                Vector3 p1 = new Vector3(i * 2 * f2m, 0, j * 2 * f2m) + offset;
+                Vector3 p2 = new Vector3(i * 2 * f2m, 0, (j + 1) * 2 * f2m) + offset;
+                Vector3 p3 = new Vector3((i + 1) * 2 * f2m, 0, (j + 1) * 2 * f2m) + offset;
+                Vector3 p4 = new Vector3((i + 1) * 2 * f2m, 0, j * 2 * f2m) + offset;
+
+                Debug.DrawLine(p1, p2, color);
+                Debug.DrawLine(p2, p3, color);
+                Debug.DrawLine(p3, p4, color);
+                Debug.DrawLine(p4, p1, color);
+            }
+        }
+    }
+}
